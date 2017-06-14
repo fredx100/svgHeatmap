713b88511d47054d2216f4ac2fa7d64d855f6676
Freddie arwen <freddiex@labcenter.com>
2017-06-14 23:51:48 +0100

    Removing debug and hooking up csv mods to svg.

--- include/svgHeatmap.js
+++ include/svgHeatmap.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var haveCSV = false;
 var haveSVG = false;
 var units = "%";
@@ -5,57 +7,37 @@ var csvObj = undefined;
 var mySvgDoc = undefined;
 
 function highlight(e) {
-  "use strict";
   this.style["stroke-width"] = 5;
   this.style.stroke = "red";
 }
 
 function lowlight(e) {
-  "use strict";
   this.style["stroke-width"] = 1;
   this.style.stroke = "black";
 }
 
-function initSvg(name) {
-  "use strict";
-  var textElem = document.getElementById("svglistener");
-  textElem.innerText = "Apply listener... (" + haveSVG + ")";
-  if (haveSVG) {
-    var elem = mySvgDoc.getElementById(name);
-    elem.addEventListener("mouseenter", highlight);
-    elem.addEventListener("mouseleave", lowlight);
-    elem.style.fill = "#0000FF";
-
-    // Debug
-    textElem = document.getElementById("svglistener");
-    textElem.innerText = "Listener applied. (" + haveSVG + ")";
-  } else {
-    textElem = document.getElementById("svglistener");
-    textElem.innerText = "Listener not applied. (" + haveSVG + ")";
-  }
-}
-
+// handleSvgFileSelect
+//
+// Pass selected SVG file to the displaying element.
 function handleSvgFileSelect(evt) {
-  "use strict";
   var file = evt.target.value; // FileList object
   var elem = document.getElementById("svgresult");
   elem.data = file;
 }
 
-function registerSVG(evt) {
+// registerSvg
+//
+// Listener for the element which displays the SVG. This globally
+// signals that we have a valid SVG by setting the global haveSVG to
+// true.
+function registerSvg(evt) {
   mySvgDoc = evt.target.contentDocument;
-  var telem = document.getElementById("svgdoc");
   if (mySvgDoc !== null) {
     haveSVG = true;
-    telem.innerText = "mySvgDoc retrieved.";
   } else {
     haveSVG = false;
-    telem.innerText = "mySvgDoc null.";
   }
-  // TODO: debug
-  telem = document.getElementById("svglistener");
-  telem.innerText = "Listening... (" + haveSVG + ")";
-  initSvg("C");
+  updateSvg();
 }
 
 // isNumeric
@@ -72,7 +54,6 @@ function isNumeric(n) {
 // The first should be defined and not zero length, while the second
 // should be numeric.
 function checkRowValid(element, index) {
-  "use strict";
   // At least two elements per row
   if ((element.length === 0) ||
     (element.length < 2)) {
@@ -97,7 +78,6 @@ function checkRowValid(element, index) {
 //
 // Checks that the CSV data is applicable and if so converts it into an object.
 function checkValidData(csvArray) {
-  "use strict";
   var status = {valid:true};
 
   csvArray.forEach(checkRowValid, status);
@@ -107,7 +87,6 @@ function checkValidData(csvArray) {
 
 // Convert an array row to an element
 function csvRowToElement(element) {
-  "use strict";
   csvObj["KEY_" + element[0]] = element[1];
   if (element[1] > csvObj.max) {
     csvObj.max = element[1];
@@ -119,7 +98,6 @@ function csvRowToElement(element) {
 
 // Convert csvArray to an Object at csvObj
 function csvArrayToObject(csvArray) {
-  "use strict";
   csvObj = {min:Infinity,max:-Infinity};
 
   csvArray.forEach(csvRowToElement);
@@ -130,22 +108,27 @@ function csvArrayToObject(csvArray) {
 }
 
 function updateSVGByKey (element) {
-  "use strict";
   if (element.startsWith("KEY_")) {
     var keystring = element.substr(4); // get key string
-    // TODO: update svg
+    var elem = mySvgDoc.getElementById(keystring);
+
+    if (elem !== null) {
+      elem.addEventListener("mouseenter", highlight);
+      elem.addEventListener("mouseleave", lowlight);
+      elem.style.fill = "#0000FF";
+      elem.title = keystring + ": " + csvObj[element] + " " + units;
+      elem.style.transparancy = (csvObj[element] - csvObj.min) * csvObj.rangeFactor;
+    }
   }
 }
 
 function updateSvg() {
-  "use strict";
   if (haveSVG && haveCSV) {
     Object.keys(csvObj).forEach(updateSVGByKey);
   }
 }
 
 function getCsvObj(file) {
-  "use strict";
   var reader = new FileReader();
   reader.onload = function(){
     var csvArray = readCSV(this.result);
@@ -163,26 +146,14 @@ function getCsvObj(file) {
 }
 
 function handleCsvFileSelect(evt) {
-  "use strict";
   var file = evt.target.files[0]; // FileList object
   getCsvObj(file); // read the file contents
 }
 
-function setRegionVal(name) {
-  "use strict";
-  var elem = mySvgDoc.getElementById(name);
-  elem.title = name + csvObj["KEY_" + name] + " " + units; // Set title
-  var opacity = (csvObj.name - csvObj.min) * csvObj.rangeFactor;
-  // TODO: use opacity as the coefficient of some colour
-  // (global?).
-  elem.style.fill = "#0000ff";
-}
-
 window.onload = function () {
-  "use strict";
   document.getElementById("csvfile").addEventListener("change", handleCsvFileSelect);
   document.getElementById("svgfile").addEventListener("change", handleSvgFileSelect);
-  document.getElementById("svgresult").addEventListener("load", registerSVG);
+  document.getElementById("svgresult").addEventListener("load", registerSvg);
 };
 
 // vim: shiftwidth=2 fdm=syntax
